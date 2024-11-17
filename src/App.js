34c2b6c3ac04a91d9b468c6d24b912eaf49c0902
Andrew Notermans <andrewnotermans@gmail.com>
2024-11-17 17:49:18 +0100

    clean-code

--- src/App.js
+++ src/App.js
@@ -1,183 +1,177 @@
 import { useEffect, useState } from "react";
 import Particles, { initParticlesEngine } from "@tsparticles/react";
 import { loadSlim } from "@tsparticles/slim";
-import './App.css';
-import Navigation from './components/Navigation/Navigation';
-import Signin from './components/Signin/Signin';
-import Register from './components/Register/Register';
-import FaceRecognition from './components/FaceRecognition/FaceRecognition';
-import Logo from './components/Logo/Logo';
-import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
-import Rank from './components/Rank/Rank';
+import "./App.css";
+import Navigation from "./components/Navigation/Navigation";
+import Signin from "./components/Signin/Signin";
+import Register from "./components/Register/Register";
+import FaceRecognition from "./components/FaceRecognition/FaceRecognition";
+import Logo from "./components/Logo/Logo";
+import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
+import Rank from "./components/Rank/Rank";
 
 function App() {
-    const [init, setInit] = useState(false);
-    const [input, setInput] = useState('');
-    const [imageUrl, setImageUrl] = useState('');
-    const [boxes, setBoxes] = useState([]);
-    const [route, setRoute] = useState('signin');
-    const [signedin, setSignedin] = useState(false);
+  const [init, setInit] = useState(false);
+  const [input, setInput] = useState("");
+  const [imageUrl, setImageUrl] = useState("");
+  const [boxes, setBoxes] = useState([]);
+  const [route, setRoute] = useState("signin");
+  const [signedin, setSignedin] = useState(false);
 
-    useEffect(() => {
-        initParticlesEngine(async (engine) => {
-            await loadSlim(engine);
-        }).then(() => {
-            setInit(true);
-        });
-    }, []);
+  useEffect(() => {
+    initParticlesEngine(async (engine) => {
+      await loadSlim(engine);
+    }).then(() => {
+      setInit(true);
+    });
+  }, []);
 
-    const MODEL_ID = 'face-detection';  
-    const returnClarifaiRequestOptions = (imageUrl) => {
-        const PAT = '60df1f2fc09b470da2117dde445f6abf';
-        const USER_ID = 'af8w21gvcx87';       
-        const APP_ID = 'image-recognition';
+  const MODEL_ID = "face-detection";
+  const returnClarifaiRequestOptions = (imageUrl) => {
+    const PAT = "60df1f2fc09b470da2117dde445f6abf";
+    const USER_ID = "af8w21gvcx87";
+    const APP_ID = "image-recognition";
 
-        const raw = JSON.stringify({
-            "user_app_id": {
-                "user_id": USER_ID,
-                "app_id": APP_ID
+    const raw = JSON.stringify({
+      user_app_id: {
+        user_id: USER_ID,
+        app_id: APP_ID,
+      },
+      inputs: [
+        {
+          data: {
+            image: {
+              url: imageUrl,
             },
-            "inputs": [
-                {
-                    "data": {
-                        "image": {
-                            "url": imageUrl
-                        }
-                    }
-                }
-            ]
-        });
-        return {
-            method: 'POST',
-            headers: {
-                'Accept': 'application/json',
-                'Authorization': 'Key ' + PAT
-            },
-            body: raw
-        };
-    };
-
-    const calculateFaceLocations = (data) => {
-        const image = document.getElementById('inputimage');
-        const width = Number(image.width);
-        const height = Number(image.height);
-
-        const regions = data.outputs[0].data.regions;
-        console.log(regions)
-        const boundingBoxes = regions.map(region => {
-            const boundingBox = region.region_info.bounding_box;
-            return {
-                leftCol: boundingBox.left_col * width,
-                topRow: boundingBox.top_row * height,
-                rightCol: width - (boundingBox.right_col * width),
-                bottomRow: height - (boundingBox.bottom_row * height),
-            };
-        });
-        return boundingBoxes;
-    };
-
-    const displayFaceBoxes = (boxes) => {
-        setBoxes(boxes);
-        console.log(boxes);
-    };
-
-    const onInputChange = (event) => {
-        setInput(event.target.value);
-    };
-
-    const onButtonSubmit = () => {
-        setImageUrl(input);
-        fetch(`https://api.clarifai.com/v2/models/${MODEL_ID}/outputs`, returnClarifaiRequestOptions(input))
-            .then(response => response.json())
-            .then(data => displayFaceBoxes(calculateFaceLocations(data)))
-            .catch(error => console.error('Error:', error));
-    };
-
-    const particlesOptions = {
-        fpsLimit: 120,
-        interactivity: {
-            events: {
-                onClick: { enable: true, mode: "push" },
-                onHover: { enable: true, mode: "repulse" },
-                resize: true,
-            },
-            modes: {
-                push: { quantity: 4 },
-                repulse: { distance: 200, duration: 0.4 },
-            },
+          },
         },
-        particles: {
-            color: { value: "#ffffff" },
-            links: {
-                color: "#ffffff",
-                distance: 150,
-                enable: true,
-                opacity: 0.5,
-                width: 1,
-            },
-            move: {
-                direction: "none",
-                enable: true,
-                outModes: { default: "bounce" },
-                random: false,
-                speed: 6,
-                straight: false,
-            },
-            number: {
-                density: { enable: true, area: 800 },
-                value: 80,
-            },
-            opacity: { value: 0.5 },
-            shape: { type: "circle" },
-            size: { value: { min: 1, max: 5 } },
-        },
-        detectRetina: true,
+      ],
+    });
+    return {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        Authorization: "Key " + PAT,
+      },
+      body: raw,
     };
+  };
 
-    const onRouteChange = (route) => {
-        if (route === 'signout') {
-            setSignedin(false);
-        } else if (route === 'home') {
-            setSignedin(true);
-        }
-        setRoute(route);
-    }
+  const calculateFaceLocations = (data) => {
+    const image = document.getElementById("inputimage");
+    const width = Number(image.width);
+    const height = Number(image.height);
 
-    return (
-        <div className="App">
-            {init && (
-                <Particles
-                    id="tsparticles"
-                    options={particlesOptions}
-                />
-            )}
-            <Navigation isSignedIn = { signedin } onRouteChange={ onRouteChange } />
-            
-            {route === 'home' ? 
-        (
-        <div>
-            <Logo />
-            <Rank />
-            <ImageLinkForm 
-                onInputChange={onInputChange} 
-                onButtonSubmit={onButtonSubmit} 
-            />
-            <FaceRecognition boxes={boxes} imageUrl={imageUrl} />
-        </div>
-    ) : route === 'signin' ? (
-        <div>
-            <Signin onRouteChange={onRouteChange} /> 
-        </div>
-    ) : (
-        <div>
-            <Register onRouteChange={onRouteChange} />
-        </div>
+    const regions = data.outputs[0].data.regions;
+    console.log(regions);
+    const boundingBoxes = regions.map((region) => {
+      const boundingBox = region.region_info.bounding_box;
+      return {
+        leftCol: boundingBox.left_col * width,
+        topRow: boundingBox.top_row * height,
+        rightCol: width - boundingBox.right_col * width,
+        bottomRow: height - boundingBox.bottom_row * height,
+      };
+    });
+    return boundingBoxes;
+  };
+
+  const displayFaceBoxes = (boxes) => {
+    setBoxes(boxes);
+    console.log(boxes);
+  };
+
+  const onInputChange = (event) => {
+    setInput(event.target.value);
+  };
+
+  const onButtonSubmit = () => {
+    setImageUrl(input);
+    fetch(
+      `https://api.clarifai.com/v2/models/${MODEL_ID}/outputs`,
+      returnClarifaiRequestOptions(input)
     )
-}
-            
-            
+      .then((response) => response.json())
+      .then((data) => displayFaceBoxes(calculateFaceLocations(data)))
+      .catch((error) => console.error("Error:", error));
+  };
+
+  const particlesOptions = {
+    fpsLimit: 120,
+    interactivity: {
+      events: {
+        onClick: { enable: true, mode: "push" },
+        onHover: { enable: true, mode: "repulse" },
+        resize: true,
+      },
+      modes: {
+        push: { quantity: 4 },
+        repulse: { distance: 200, duration: 0.4 },
+      },
+    },
+    particles: {
+      color: { value: "#ffffff" },
+      links: {
+        color: "#ffffff",
+        distance: 150,
+        enable: true,
+        opacity: 0.5,
+        width: 1,
+      },
+      move: {
+        direction: "none",
+        enable: true,
+        outModes: { default: "bounce" },
+        random: false,
+        speed: 6,
+        straight: false,
+      },
+      number: {
+        density: { enable: true, area: 800 },
+        value: 80,
+      },
+      opacity: { value: 0.5 },
+      shape: { type: "circle" },
+      size: { value: { min: 1, max: 5 } },
+    },
+    detectRetina: true,
+  };
+
+  const onRouteChange = (route) => {
+    if (route === "signout") {
+      setSignedin(false);
+    } else if (route === "home") {
+      setSignedin(true);
+    }
+    setRoute(route);
+  };
+
+  return (
+    <div className="App">
+      {init && <Particles id="tsparticles" options={particlesOptions} />}
+      <Navigation isSignedIn={signedin} onRouteChange={onRouteChange} />
+
+      {route === "home" ? (
+        <div>
+          <Logo />
+          <Rank />
+          <ImageLinkForm
+            onInputChange={onInputChange}
+            onButtonSubmit={onButtonSubmit}
+          />
+          <FaceRecognition boxes={boxes} imageUrl={imageUrl} />
         </div>
-    );
+      ) : route === "signin" ? (
+        <div>
+          <Signin onRouteChange={onRouteChange} />
+        </div>
+      ) : (
+        <div>
+          <Register onRouteChange={onRouteChange} />
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default App;
--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -1,26 +1,31 @@
-import React from 'react';
-import './FaceRecognition.css';
+import React from "react";
+import "./FaceRecognition.css";
 
 const FaceRecognition = ({ imageUrl, boxes }) => {
-    return (
-        <div className='center ma'>
-            <div className='absolute mt2'>
-                <img id='inputimage' src={imageUrl} alt="" style={{ width: '500px', height: 'auto' }} />
-                {boxes.map((box, index) => (
-                    <div
-                        key={index}
-                        className='bounding-box'
-                        style={{
-                            top: box.topRow,
-                            right: box.rightCol,
-                            bottom: box.bottomRow,
-                            left: box.leftCol
-                        }}
-                    ></div>
-                ))}
-            </div>
-        </div>
-    );
+  return (
+    <div className="center ma">
+      <div className="absolute mt2">
+        <img
+          id="inputimage"
+          src={imageUrl}
+          alt=""
+          style={{ width: "500px", height: "auto" }}
+        />
+        {boxes.map((box, index) => (
+          <div
+            key={index}
+            className="bounding-box"
+            style={{
+              top: box.topRow,
+              right: box.rightCol,
+              bottom: box.bottomRow,
+              left: box.leftCol,
+            }}
+          ></div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default FaceRecognition;
