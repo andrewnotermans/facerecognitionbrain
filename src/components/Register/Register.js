9e8cdad16814456feb470ac34484be5c9e65e419
Andrew Notermans <andrewnotermans@gmail.com>
2024-11-26 23:15:24 +0100

    Revert "change-type-button"
    This reverts commit 0d28c0e53b57ca0e012a9844582d7a23b98586f4.

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -87,7 +87,7 @@ const Register = ({ loadUser, onRouteChange }) => {
             <input
               onClick={onSubmitSignIn}
               className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib"
-              type="button"
+              type="submit"
               value="Register"
             />
           </div>
